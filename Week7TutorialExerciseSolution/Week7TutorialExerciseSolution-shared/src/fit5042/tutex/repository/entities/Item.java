04e900ca67bce07ae28e38b941b09cbe265ea3dd
thinking_fioa <thinking_fioa@163.com>
2018-08-04 19:11:47 +0800

    To List

--- Week7TutorialExerciseSolution/Week7TutorialExerciseSolution-shared/src/fit5042/tutex/repository/entities/Item.java
+++ Week7TutorialExerciseSolution/Week7TutorialExerciseSolution-shared/src/fit5042/tutex/repository/entities/Item.java
@@ -16,10 +16,16 @@ import javax.persistence.NamedQuery;
  * @author Eddie
  */
 @Entity
-@NamedQueries({@NamedQuery(name = Item.GET_ALL_QUERY_NAME, query = "SELECT i FROM Item i")})
+@NamedQueries({
+    @NamedQuery(name = Item.GET_ALL_QUERY_NAME, query = "SELECT i FROM Item i"),
+    @NamedQuery(name = Item.FIND_BY_TITLE, query = "SELECT i FROM Item i where i.title = :title"),
+    @NamedQuery(name = Item.SEARCH_BY_LABEL, query = "SELECT i FROM Item i where i.labels = :labels")
+})
 public class Item implements Serializable {
     
     public static final String GET_ALL_QUERY_NAME = "Item.getAll";
+    public static final String FIND_BY_TITLE = "Item.findByTitle";
+    public static final String SEARCH_BY_LABEL = "Item.searchByLabel";
     
     private int itemId; 
     private String title;
