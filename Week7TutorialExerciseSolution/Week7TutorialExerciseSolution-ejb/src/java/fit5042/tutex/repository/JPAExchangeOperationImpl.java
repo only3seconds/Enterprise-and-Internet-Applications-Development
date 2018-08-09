6df9baa46e4b85657c91a47c589c61c52b05afcf
thinking_fioa <thinking_fioa@163.com>
2018-08-09 23:06:22 +0800

    subOrder

--- Week7TutorialExerciseSolution/Week7TutorialExerciseSolution-ejb/src/java/fit5042/tutex/repository/JPAExchangeOperationImpl.java
+++ Week7TutorialExerciseSolution/Week7TutorialExerciseSolution-ejb/src/java/fit5042/tutex/repository/JPAExchangeOperationImpl.java
@@ -7,6 +7,7 @@ package fit5042.tutex.repository;
 
 import fit5042.tutex.repository.entities.Exchange;
 import fit5042.tutex.repository.entities.Item;
+import fit5042.tutex.repository.entities.SubOrder;
 import fit5042.tutex.repository.entities.SysUser;
 import java.util.HashSet;
 import java.util.List;
@@ -31,5 +32,28 @@ public class JPAExchangeOperationImpl implements ExchangeOperation {
         List<Exchange> exchanges =  entityManager.createNamedQuery(Exchange.FIND_BY_USERID, Exchange.class).setParameter("userId", userId).getResultList();
         System.out.println(exchanges);
         return exchanges;
+    }
+    
+    @Override
+    public void insertSubOrder(List<SubOrder> orderList){
+        for(SubOrder order: orderList) {
+            entityManager.persist(order);
+        }
+    }
+    
+    /**
+     * 返回交易Id,也就是: exchangeId
+     * @param exchange
+     * @return 
+     */
+    @Override
+    public int insertExchange(Exchange exchange) {
+        entityManager.persist(exchange);
+        List<Exchange> exchanges =  entityManager.createNamedQuery(Exchange.FIND_BY_USERID, Exchange.class).setParameter("userId", exchange.getSysuserSysUserId()).getResultList();
+        int exchangeId = -1;
+        for(Exchange exchange1 : exchanges) {
+            exchangeId = Integer.max(exchange1.getExchangeId(), exchangeId);
+        }
+        return exchangeId;
     }
 }
